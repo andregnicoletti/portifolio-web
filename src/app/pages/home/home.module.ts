ff435b5af8b881342d10df01b0a9ded013718e00
André Nicoletti <andregnicoletti@gmail.com>
2025-06-17 12:16:18 -0300

    feat: git service

--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -1,11 +1,24 @@
-import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { NgModule, OnInit } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { GithubService } from 'src/app/services/github.service';
 import { SharedModule } from 'src/app/shared/shared.module';
+import { Repository } from 'src/app/types/repository.interface';
 
 @NgModule({
   declarations: [],
-  imports: [BrowserModule, SharedModule],
+  imports: [BrowserModule, SharedModule, HttpClientModule],
   providers: [],
   bootstrap: [],
 })
-export class HomeModule {}
+export class HomeModule implements OnInit {
+  repositories: Repository[] = [];
+
+  constructor(private gitHubService: GithubService) {}
+
+  ngOnInit(): void {
+    this.gitHubService.getRepositories().subscribe({
+      next: (response) => (this.repositories = response.body ?? []),
+    });
+  }
+}
